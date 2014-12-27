eb4259b6814f132c3dd8d380bed789a3ef863230
lenhathuy <lenhathuy264@gmail.com>
2014-12-27 15:11:35 +0700

    Revert "Revert "fdsaf""
    This reverts commit 9ac9ec340bb9335f4cf72767c27c1e462fc85f90.

---
+++ Meganet/frmRegister.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Meganet
+{
+    partial class FrmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtRePass = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(83, 132);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(232, 20);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.Enter += new System.EventHandler(this.txtEmail_Enter);
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(83, 205);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(232, 20);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.Enter += new System.EventHandler(this.txtPass_Enter);
+            this.txtPass.Leave += new System.EventHandler(this.txtPass_Leave);
+            // 
+            // txtRePass
+            // 
+            this.txtRePass.Location = new System.Drawing.Point(83, 270);
+            this.txtRePass.Name = "txtRePass";
+            this.txtRePass.Size = new System.Drawing.Size(232, 20);
+            this.txtRePass.TabIndex = 2;
+            this.txtRePass.Enter += new System.EventHandler(this.txtRePass_Enter);
+            this.txtRePass.Leave += new System.EventHandler(this.txtRePass_Leave);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(106, 329);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(149, 23);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Create an acount";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // FrmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 379);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtRePass);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "FrmRegister";
+            this.Text = "frmRegister";
+            this.Load += new System.EventHandler(this.frmRegister_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtRePass;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
